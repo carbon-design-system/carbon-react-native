c1a4863fa7e9089357b8e067e5d55a58b16d30fa
David Bradshaw <david.bradshaw@ibm.com>
2022-08-24 19:02:22 -0700

    fix(#18): small tweaks to UI panel

--- example/src/Views/UiPanel.tsx
+++ example/src/Views/UiPanel.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { StyleSheet, ScrollView, Alert } from 'react-native';
-import { UiPanel, UiPanelItemProps, Checkbox, Button } from 'carbon-react-native';
+import { UiPanel, UiPanelItemProps, Checkbox, ViewWrapper, TopNavigationBar } from 'carbon-react-native';
 import ApertureIcon from '@carbon/icons/es/aperture/20';
 import LaunchIcon from '@carbon/icons/es/launch/20';
+import MenuIcon from '@carbon/icons/es/menu/20';
+import CloseIcon from '@carbon/icons/es/close/20';
+import ExitIcon from '@carbon/icons/es/logout/20';
 
 const styles = StyleSheet.create({
   view: {
@@ -15,16 +18,22 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class TestUiPanel extends React.Component {
+export default class TestUiPanel extends React.Component<{
+  goHome: () => void;
+}> {
   state = {
     open: false,
     hasLeftIcon: false,
     hasRightIcon: false,
+    hasChildLeftIcon: false,
+    hasChildRightIcon: false,
     hideFourthItem: false,
+    autoCloseNoChild: false,
   };
 
-  private openPanel = (): void => {
-    this.setState({ open: true });
+  private togglePanel = (): void => {
+    const { open } = this.state;
+    this.setState({ open: !open });
   };
 
   private alert = (text: string): void => {
@@ -32,21 +41,24 @@ export default class TestUiPanel extends React.Component {
   };
 
   private get itemProps(): UiPanelItemProps[] {
-    const { hasLeftIcon, hasRightIcon, hideFourthItem } = this.state;
+    const { hasLeftIcon, hasRightIcon, hasChildLeftIcon, hasChildRightIcon, hideFourthItem } = this.state;
 
     return [
       {
         text: 'Item 1: Toggle to expand',
         leftIcon: hasLeftIcon ? ApertureIcon : undefined,
+        rightIcon: hasRightIcon ? ApertureIcon : undefined,
         children: [
           {
             text: 'Children 1',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 1 / Children 1'),
           },
           {
             text: 'Children 2',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildRightIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildLeftIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 1 / Children 2'),
           },
         ],
@@ -54,12 +66,14 @@ export default class TestUiPanel extends React.Component {
       {
         text: 'Item 2: Disabled',
         leftIcon: hasLeftIcon ? ApertureIcon : undefined,
+        rightIcon: hasRightIcon ? ApertureIcon : undefined,
         textBreakMode: 'tail',
         disabled: true,
         children: [
           {
             text: 'Children 1',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 2 / Children 1'),
           },
         ],
@@ -67,17 +81,20 @@ export default class TestUiPanel extends React.Component {
       {
         text: 'Item 3: Opened on load',
         leftIcon: hasLeftIcon ? ApertureIcon : undefined,
+        rightIcon: hasRightIcon ? ApertureIcon : undefined,
         openOnLoad: true,
         children: [
           {
             text: 'Children 1: Disabled',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 1 / Children 1'),
             disabled: true,
           },
           {
             text: 'Children 2',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 1 / Children 2'),
             onLongPress: () => this.alert('Long pressed Item 1 / Children 2'),
           },
@@ -86,28 +103,33 @@ export default class TestUiPanel extends React.Component {
       {
         text: 'Item 4: Uncheck to hide',
         leftIcon: hasLeftIcon ? ApertureIcon : undefined,
+        rightIcon: hasRightIcon ? ApertureIcon : undefined,
         hidden: hideFourthItem,
         children: [
           {
             text: 'Children 1',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 4 / Children 1'),
           },
           {
             text: 'Children 2: Disabled',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             disabled: true,
             onPress: () => this.alert('Pressed Item 4 / Children 2'),
           },
           {
             text: 'Children 3',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             hidden: true,
             onPress: () => this.alert('Pressed Item 4 / Children 3'),
           },
           {
             text: 'Children 4: Children 3 is hidden',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 4 / Children 4'),
           },
         ],
@@ -115,49 +137,62 @@ export default class TestUiPanel extends React.Component {
       {
         text: 'Item 5: Long text with ellipsis instead of wrapped',
         leftIcon: hasLeftIcon ? ApertureIcon : undefined,
+        rightIcon: hasRightIcon ? ApertureIcon : undefined,
         textBreakMode: 'tail',
         children: [
           {
             text: 'Children 1',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 5 / Children 1'),
           },
           {
             text: 'Children 2: Long text with ellipsis instead of wrapped',
             textBreakMode: 'tail',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 5 / Children 2'),
           },
           {
             text: 'Children 3: Long text with normal wrapped',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 5 / Children 3'),
           },
           {
             text: 'Children 4',
-            rightIcon: hasRightIcon ? LaunchIcon : undefined,
+            leftIcon: hasChildLeftIcon ? LaunchIcon : undefined,
+            rightIcon: hasChildRightIcon ? LaunchIcon : undefined,
             onPress: () => this.alert('Pressed Item 5 / Children 4'),
           },
         ],
       },
       {
         text: 'Item 6: No nested items',
+        rightIcon: hasRightIcon ? ApertureIcon : undefined,
         leftIcon: hasLeftIcon ? ApertureIcon : undefined,
+        onPress: () => this.alert('Pressed Item 6'),
       },
     ];
   }
 
   render(): React.ReactNode {
-    const { open, hasLeftIcon, hasRightIcon, hideFourthItem } = this.state;
+    const { open, hasLeftIcon, hasRightIcon, hideFourthItem, hasChildLeftIcon, hasChildRightIcon, autoCloseNoChild } = this.state;
+    const { goHome } = this.props;
 
     return (
-      <ScrollView keyboardShouldPersistTaps="handled" contentInsetAdjustmentBehavior="automatic" contentContainerStyle={styles.container} style={styles.view}>
-        <Checkbox checked={hasLeftIcon} id="hasLeftIcon" onPress={(value) => this.setState({ hasLeftIcon: value })} label="Add left icon at top level" />
-        <Checkbox checked={hasRightIcon} id="hasRightIcon" onPress={(value) => this.setState({ hasRightIcon: value })} label="Add right icon in the nested level" />
-        <Checkbox checked={hideFourthItem} id="hideFourthItem" onPress={(value) => this.setState({ hideFourthItem: value })} label="Check to hide the 4th item" />
-        <Button onPress={this.openPanel} text="Open Panel" />
-        <UiPanel open={open} onClose={() => this.setState({ open: false })} items={this.itemProps} />
-      </ScrollView>
+      <ViewWrapper hasTopNavigation={true}>
+        <TopNavigationBar title="UI panel" leftItems={[{ text: 'Toggle menu', icon: open ? CloseIcon : MenuIcon, onPress: this.togglePanel, active: open }]} rightItems={[{ text: 'Return to components', icon: ExitIcon, onPress: goHome }]} />
+        <ScrollView keyboardShouldPersistTaps="handled" contentInsetAdjustmentBehavior="automatic" contentContainerStyle={styles.container} style={styles.view}>
+          <Checkbox checked={hasLeftIcon} id="hasLeftIcon" onPress={(value) => this.setState({ hasLeftIcon: value })} label="Add left icon at top level" />
+          <Checkbox checked={hasRightIcon} id="hasRightIcon" onPress={(value) => this.setState({ hasRightIcon: value })} label="Add right icon at top level" />
+          <Checkbox checked={hasChildRightIcon} id="hasChildRightIcon" onPress={(value) => this.setState({ hasChildRightIcon: value })} label="Add right icon in the nested level" />
+          <Checkbox checked={hasChildLeftIcon} id="hasChildLeftIcon" onPress={(value) => this.setState({ hasChildLeftIcon: value })} label="Add left icon in the nested level" />
+          <Checkbox checked={hideFourthItem} id="hideFourthItem" onPress={(value) => this.setState({ hideFourthItem: value })} label="Check to hide the 4th item" />
+          <Checkbox checked={autoCloseNoChild} id="autoCloseNoChild" onPress={(value) => this.setState({ autoCloseNoChild: value })} label="Close when pressing item with no children" />
+        </ScrollView>
+        <UiPanel open={open} onClose={() => this.setState({ open: false })} items={this.itemProps} closeOnNoChildrenPress={autoCloseNoChild} />
+      </ViewWrapper>
     );
   }
 }
--- src/components/UiPanel/index.tsx
+++ src/components/UiPanel/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { ViewProps, StyleProp, StyleSheet, ViewStyle, ScrollView, Modal as ReactModal, SafeAreaView, Pressable } from 'react-native';
+import { ViewProps, StyleProp, StyleSheet, ViewStyle, ScrollView, Modal as ReactModal, SafeAreaView, Pressable, View } from 'react-native';
 import { getColor } from '../../styles/colors';
 import { styleReferenceBreaker } from '../../helpers';
 import { UiPanelItem, UiPanelItemProps } from '../UiPanelItem';
 import { modalPresentations } from '../../constants/constants';
-import { Overlay } from '../Overlay';
 import { BottomSafeAreaColorOverride } from '../BottomSafeAreaColorOverride';
+import { defaultText } from '../../constants/defaultText';
+import { zIndexes } from '../../styles/z-index';
 
 export type UiPanelProps = {
   /** Indicate if the panel should be open */
@@ -14,6 +15,10 @@ export type UiPanelProps = {
   onClose: () => void;
   /** List of top level items to render */
   items: UiPanelItemProps[];
+  /** Text to use for on close areas (accessibility). Defaults to ENGLISH "Close" */
+  onCloseText?: string;
+  /** Indicate if should close if pressing item with no children */
+  closeOnNoChildrenPress?: boolean;
   /** Custom style to set on the item */
   style?: StyleProp<ViewStyle>;
   /** Direct props to set on the React Native component (including iOS and Android specific props). Most use cases should not need this. */
@@ -27,53 +32,52 @@ export class UiPanel extends React.Component<UiPanelProps> {
         position: 'relative',
         flexGrow: 1,
       },
-      blurBackground: {
-        zIndex: -1,
-        position: 'absolute',
-        top: 96,
-        right: 0,
-        left: 0,
-        bottom: 0,
-        flex: 1,
+      innerWrapping: {
+        position: 'relative',
+        flexGrow: 1,
       },
       pressableTop: {
         height: 48,
-        width: '100%',
       },
       pressableRight: {
-        width: 48,
+        zIndex: zIndexes.behind,
+        backgroundColor: getColor('overlay'),
         position: 'absolute',
-        top: 96,
+        top: 48,
         right: 0,
         bottom: 0,
-        flex: 1,
+        left: 0,
       },
       panelWrapper: {
+        flex: 1,
         backgroundColor: getColor('layer01'),
-        height: 192,
         marginRight: 48,
+      },
+      panelWrapperInner: {
+        paddingBottom: 48,
       },
     });
   }
 
   render(): React.ReactNode {
-    const { open, items, style, componentProps, onClose } = this.props;
+    const { open, items, style, componentProps, onClose, onCloseText, closeOnNoChildrenPress } = this.props;
 
     return (
       open && (
         <ReactModal supportedOrientations={modalPresentations} transparent={true}>
-          <Overlay style={this.styles.blurBackground} />
           <BottomSafeAreaColorOverride color={getColor('layer01')} marginRight={48} />
           <SafeAreaView style={this.styles.safeAreaWrapper}>
-            <Pressable style={this.styles.pressableTop} onPress={() => onClose()} />
-            <Pressable style={this.styles.pressableRight} onPress={() => onClose()} />
-            <ScrollView bounces={false} style={styleReferenceBreaker(this.styles.panelWrapper, style)} accessibilityRole="menu" {...(componentProps || {})}>
-              {(items || [])
-                .filter((item) => !item.hidden)
-                .map((item, index) => (
-                  <UiPanelItem key={index} {...item} />
-                ))}
-            </ScrollView>
+            <View style={this.styles.innerWrapping}>
+              <Pressable style={this.styles.pressableTop} accessibilityRole="button" accessibilityLabel={onCloseText || defaultText.close} onPress={onClose} />
+              <Pressable style={this.styles.pressableRight} accessibilityRole="button" accessibilityLabel={onCloseText || defaultText.close} onPress={onClose} />
+              <ScrollView bounces={false} style={styleReferenceBreaker(this.styles.panelWrapper, style)} contentContainerStyle={this.styles.panelWrapperInner} accessibilityRole="menu" {...(componentProps || {})}>
+                {(items || [])
+                  .filter((item) => !item.hidden)
+                  .map((item, index) => (
+                    <UiPanelItem key={index} {...item} noChildrenPressCallback={closeOnNoChildrenPress ? onClose : undefined} />
+                  ))}
+              </ScrollView>
+            </View>
           </SafeAreaView>
         </ReactModal>
       )
