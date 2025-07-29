b9b02301de86df571f208fdc70f6e0f45d73d7eb
David Bradshaw <dabrad26@users.noreply.github.com>
2025-07-29 11:38:49 -0700

    fix: lint and test fix for babel and recent changes (#232)

--- src/components/BaseTextInputs/index.tsx
+++ src/components/BaseTextInputs/index.tsx
@@ -61,7 +61,7 @@ export const getTextInputStyle = (light?: boolean, hasLabelLink?: boolean, fullB
     borderColor: getColor('field01'),
     color: getColor('textPrimary'),
     borderBottomColor: getColor('borderStrong02'),
-    borderWidth: Platform.select({ios: 2, android: 0}),
+    borderWidth: Platform.select({ ios: 2, android: 0 }),
     borderBottomWidth: 1,
     paddingRight: 16,
     paddingLeft: 18,
